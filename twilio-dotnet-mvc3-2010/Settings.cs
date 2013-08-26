a2ec95009d30c000e53c4cd0eb7ebd82da19aa64
Devin Rader <devin@twilio.com>
2013-08-26 17:37:01 -0400

    Added MVC3 template

--- twilio-dotnet-mvc3-2010/Settings.cs
+++ twilio-dotnet-mvc3-2010/Settings.cs
@@ -7,8 +7,8 @@ namespace twilio_dotnet
 {
     public static class Settings
     {
-        public static string AccountSid { get { return "AC3137d76457814a5eabf7de62f346d39a"; } }
-        public static string AuthToken { get { return "01e8896765b4c7798e0f9d888948a9b2"; } }
-        public static string TwilioNumber { get { return "+13168545311"; } }
+        public static string AccountSid { get { return "CHANGE_ME"; } }
+        public static string AuthToken { get { return "CHANGE_ME"; } }
+        public static string TwilioNumber { get { return "CHANGE_ME"; } }
     }
 }
